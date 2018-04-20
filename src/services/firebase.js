4099bfb50be95e2fb742c84aa9fdc68e336e793a
Yoshuro <yoshuro@hotmail.com>
2018-04-20 19:44:05 +0200

    Add firebase service

---
+++ src/services/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyBR0wufY7mjLxD68v5ctievTvANT_8vtlg",
+  authDomain: "celestial-e65d5.firebaseapp.com",
+  databaseURL: "https://celestial-e65d5.firebaseio.com",
+  projectId: "celestial-e65d5",
+  storageBucket: "",
+  messagingSenderId: "50419330644"
+}
+
+firebase.initializeApp(config)
+
+export default firebase
