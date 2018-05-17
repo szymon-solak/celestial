19f8138927669a0e6d3baf75f90a0dc2e87afab0
Yoshuro <yoshuro@hotmail.com>
2018-05-17 20:35:24 +0200

    Create skeleton of the Add page

---
+++ src/pages/add/components/addBlockBtn.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import FontAwesome from '@fortawesome/react-fontawesome'
+import faPlus from '@fortawesome/fontawesome-free-solid/faPlus'
+
+import BaseBtn from '../../../components/button/base'
+
+const Button = BaseBtn.extend`
+  width: 100%;
+  min-height: 8em;
+
+  background: ${props => props.theme.background};
+  color: ${props => props.theme.focus};
+
+  transition: all .2s linear;
+
+  &:hover {
+    background: ${props => props.theme.focus};
+    color: ${props => props.theme.fontFocus};
+  }
+`
+
+const AddBlockBtn = ({ onClick }) => (
+  <Button onClick={onClick}>
+    <FontAwesome icon={faPlus} />
+  </Button>
+)
+
+AddBlockBtn.propTypes = {
+  onClick: PropTypes.func,
+}
+
+export default AddBlockBtn
--- src/pages/add/index.js
+++ src/pages/add/index.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Section from '../../components/section'
+import Title from '../../components/title'
 
-const Add = () => (
-  <Section>Add</Section>
-)
+import AddBlockBtn from './components/addBlockBtn'
+
+class Add extends Component {
+  state = {
+    blocks: [],
+  }
+
+  handleAddClick = () => {
+    console.log('Add')
+  }
+
+  render() {
+    return (
+      <Section>
+        <Title center>Add a new entry</Title>
+        <AddBlockBtn onClick={this.handleAddClick}/>
+      </Section>
+    )
+  }
+}
 
 export default Add
